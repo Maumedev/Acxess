f874f5ffb2a7753997834456e0df6e19f9cde82a
Mauro Mendez <mauromendezme@gmail.com.com>
2025-12-30 16:41:57 -0600

    feat(Marketing): apply global query filter idTenant

--- src/modules/Acxess.Marketing/Infrastructure/Persistence/MarketingModuleContext.cs
+++ src/modules/Acxess.Marketing/Infrastructure/Persistence/MarketingModuleContext.cs
@@ -1,10 +1,14 @@
 using System.Reflection;
+using Acxess.Infrastructure.Extensions;
 using Acxess.Marketing.Domain.Entities;
+using Acxess.Shared.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Acxess.Marketing.Infrastructure.Persistence;
 
-public class MarketingModuleContext(DbContextOptions<MarketingModuleContext> options) : DbContext(options)
+public class MarketingModuleContext(
+    DbContextOptions<MarketingModuleContext> options,
+    ICurrentTenant current) : DbContext(options)
 {
     public DbSet<Promotion> Promotions { get; set; }
     public DbSet<Coupon> Coupons { get; set; }
@@ -16,5 +20,7 @@ public class MarketingModuleContext(DbContextOptions<MarketingModuleContext> opt
         modelBuilder.HasDefaultSchema("Marketing");
 
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.ApplyTenantFilters(current);
     }
 }
