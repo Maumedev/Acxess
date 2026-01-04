f89024352dc83162fbdce6bb8008df103f831b4c
Mauro Mendez <mauromendezme@gmail.com.com>
2026-01-03 21:42:41 -0600

    feat(Web): apply styles

--- src/Acxess.Web/wwwroot/js/site.js
+++ src/Acxess.Web/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('alpine:init', () => {
+    Alpine.data('layout', () => ({
+        sidebarOpen: false, // Controla el sidebar en Móvil
+        darkMode: localStorage.getItem('theme') === 'dark',
+
+        init() {
+            // Inicializar tema
+            if (this.darkMode) document.documentElement.classList.add('dark');
+            else document.documentElement.classList.remove('dark');
+        },
+
+        toggleSidebar() {
+            this.sidebarOpen = !this.sidebarOpen;
+        },
+
+        toggleTheme() {
+            this.darkMode = !this.darkMode;
+            localStorage.setItem('theme', this.darkMode ? 'dark' : 'light');
+            if (this.darkMode) document.documentElement.classList.add('dark');
+            else document.documentElement.classList.remove('dark');
+        }
+    }));
+});
